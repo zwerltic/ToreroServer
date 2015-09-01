e2c5c5c1b57062f0082ee239696e9528e310f5e6
zwerltic <zwerltic@gmail.com>
2015-09-01 04:49:47 -0500

    Updated to mail sender

--- server.js
+++ server.js
@@ -2,9 +2,39 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var http = require('http');
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'zwerltic@gmail.com',
+        pass: '1916zweRltic'
+    }
+});
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'Torero Server <zwerltic@gmail.com>', // sender address
+    to: 'jose.tlacuilo@gmail.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ✔', // plaintext body
+    html: '<b>Hello world ✔</b>' // html body
+};
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+
+      });
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	  response.write("Welcome to Node.js on OpenShift!\n\n");
 	  response.end("Thanks for visiting us! \n");
