75516da450854f78ab2cc38de5cdbb9a9caae7bb
zwerltic <zwerltic@gmail.com>
2015-09-12 00:05:34 -0500

    ef

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var http = require('http');
 var nodemailer = require('nodemailer');
 var conekta = require('conekta');
+var dispatcher = require('httpdispatcher');
 
 conekta.api_key = 'key_EfsX62HbiTSNsuHr5q6xv2Q';
 conekta.locale = 'es';
@@ -23,56 +24,64 @@ var transporter = nodemailer.createTransport({
 // setup e-mail data with unicode symbols
 var mailOptions = {
     from: 'Torero Server <toreroapp@gmail.com>', // sender address
-    to: 'toreroapp@gmail.com ,jose.tlacuilo@gmail.com, zwerltic@gmail.com', // list of receivers
+    to: 'zwerltic@gmail.com', // list of receivers
     subject: 'Hello ✔', // Subject line
     text: 'Hello world ✔', // plaintext body
     html: '<b>Hello world ✔</b>' // html body
 };
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url );
-    // send mail with defined transport object
-    // conekta.Charge.create({
-    //   amount: 250000,
-    //   currency: "MXN",
-    //   description: "tramite de amparo",
-    //   reference_id: "internal_order_id",
-    //   card: "tok_test_visa_4242",
-    //   details: {
-    //     email: "toreroapp@gmail.com",
-    //     line_items: [{
-    //       name: "Amparo",
-    //       sku: "amp_s1",
-    //       unit_price: 250000,
-    //       description: "Mexico.",
-    //       quantity: 1,
-    //       type: "laws-purchase"
-    //     }]
-    //   }
-    //   }, function(res) {
-    //     console.log(res.toObject());
-    //     transporter.sendMail(mailOptions, function(error, info){
-    //       if(error){
-    //           response.write("Message gone wrong");
-    //           return console.log(error);
-    //         }
-    //           console.log('Message sent: ' + info.response);
-    //           response.write("message sent");
-    //
-    //       });
-    //   }, function(err) {
-    //     console.log(err.message_to_purchaser);
-    //   });
-
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	  response.write("Welcome to Node.js on OpenShift!\n\n");
-	  response.end("Thanks for visiting us! \n");
-});
+var server = http.createServer(handleRequest);
 
 server.listen( port, ipaddress, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
+
+    console.log("Listening to " + ipaddress + ":" + port + "...");
 });
 
+//Lets use our dispatcher
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
 
+//A sample POST request
+dispatcher.onPost("/charge", function(req, res) {
+    conekta.Charge.create({
+      amount: 250000,
+      currency: "MXN",
+      description: "tramite de amparo",
+      reference_id: "internal_order_id",
+      card: "tok_test_visa_4242",
+      details: {
+        email: "toreroapp@gmail.com",
+        line_items: [{
+          name: "Amparo",
+          sku: "amp_s1",
+          unit_price: 250000,
+          description: "Mexico.",
+          quantity: 1,
+          type: "laws-purchase"
+        }]
+      }
+      }, function(res) {
+        console.log(res.toObject());
+        transporter.sendMail(mailOptions, function(error, info){
+          if(error){
+              response.write("Message gone wrong");
+              return console.log(error);
+            }
+              console.log('Message sent: ' + info.response);
+              response.write("message sent");
 
-console.log("Listening to " + ipaddress + ":" + port + "...");
+          });
+      }, function(err) {
+        console.log(err.message_to_purchaser);
+      });
+    res.end('Got Post Data');
+});
